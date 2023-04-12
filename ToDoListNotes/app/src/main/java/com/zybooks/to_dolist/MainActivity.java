829e5db7e37ed1deca53b4f2c3219d4ab8ced052
tyler-hungerrr <107515079+tyler-hungerrr@users.noreply.github.com>
2023-04-11 20:44:18 -0400

    new base

---
+++ ToDoListNotes/app/src/main/java/com/zybooks/to_dolist/MainActivity.java
@@ -0,0 +1,87 @@
+package com.zybooks.to_dolist;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToDoList mToDoList;
+    private EditText mItemEditText;
+    private TextView mItemListTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mItemEditText = findViewById(R.id.todo_item);
+        mItemListTextView = findViewById(R.id.item_list);
+
+        findViewById(R.id.add_button).setOnClickListener(view -> addButtonClick());
+        findViewById(R.id.clear_button).setOnClickListener(view -> clearButtonClick());
+
+        mToDoList = new ToDoList(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        try {
+            // Attempt to load a previously saved list
+            mToDoList.readFromFile();
+            displayList();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        try {
+            // Save list for later
+            mToDoList.saveToFile();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void addButtonClick() {
+
+        // Ignore any leading or trailing spaces
+        String item = mItemEditText.getText().toString().trim();
+
+        // Clear the EditText so it's ready for another item
+        mItemEditText.setText("");
+
+        // Add the item to the list and display it
+        if (item.length() > 0) {
+            mToDoList.addItem(item);
+            displayList();
+        }
+    }
+
+    private void displayList() {
+
+        // Display a numbered list of items
+        StringBuffer itemText = new StringBuffer();
+        String[] items = mToDoList.getItems();
+        for (int i = 0; i < items.length; i++) {
+            itemText.append(i + 1).append(". ").append(items[i]).append("\n");
+        }
+
+        mItemListTextView.setText(itemText);
+    }
+
+    private void clearButtonClick() {
+        mToDoList.clear();
+        displayList();
+    }
+}
