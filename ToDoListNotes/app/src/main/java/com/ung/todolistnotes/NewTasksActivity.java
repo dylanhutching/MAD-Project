30a7f4ed0f2e70055b46da7b4298f7f73d305e36
colejarrett <99043334+colejarrett@users.noreply.github.com>
2023-04-12 19:58:45 -0400

    new task and new category pages
    category page still needs color picker

---
+++ ToDoListNotes/app/src/main/java/com/ung/todolistnotes/NewTasksActivity.java
@@ -0,0 +1,14 @@
+package com.ung.todolistnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewTasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tasks);
+    }
+}
