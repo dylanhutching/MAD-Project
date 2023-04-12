460a76a5edc560ea67d56454fbc944a05e53e047
tyler-hungerrr <107515079+tyler-hungerrr@users.noreply.github.com>
2023-04-11 21:38:53 -0400

    New basic version

---
+++ ToDoListNotes/app/src/main/java/com/ung/todolistnotes/ToDoList.java
@@ -0,0 +1,70 @@
+package com.ung.todolistnotes;
+
+import android.content.Context;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+
+    public static final String FILENAME = "todolist.txt";
+
+    private Context mContext;
+    private List<String> mTaskList;
+
+    public ToDoList(Context context) {
+        mContext = context;
+        mTaskList = new ArrayList<>();
+    }
+
+    public void addItem(String item) throws IllegalArgumentException {
+        mTaskList.add(item);
+    }
+
+    public String[] getItems() {
+        String[] items = new String[mTaskList.size()];
+        return mTaskList.toArray(items);
+    }
+
+    public void clear() {
+        mTaskList.clear();
+    }
+
+    public void saveToFile() throws IOException {
+
+        // Write list to file in internal storage
+        FileOutputStream outputStream = mContext.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+        writeListToStream(outputStream);
+    }
+
+    public void readFromFile() throws IOException {
+
+        // Read in list from file in internal storage
+        FileInputStream inputStream = mContext.openFileInput(FILENAME);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            mTaskList.clear();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                mTaskList.add(line);
+            }
+        }
+        catch (FileNotFoundException ex) {
+            // Ignore
+        }
+    }
+
+    private void writeListToStream(FileOutputStream outputStream) {
+        PrintWriter writer = new PrintWriter(outputStream);
+        for (String item : mTaskList) {
+            writer.println(item);
+        }
+        writer.close();
+    }
+}
