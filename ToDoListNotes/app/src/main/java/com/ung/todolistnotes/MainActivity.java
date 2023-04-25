9a4b413915ead964f662c71eba05d1a6dde8d2b7
dylanhutching <123214388+dylanhutching@users.noreply.github.com>
2023-04-25 02:10:40 -0400

    Fix to the previous merge, accidentally deleted some of my changes.

--- ToDoListNotes/app/src/main/java/com/ung/todolistnotes/MainActivity.java
+++ ToDoListNotes/app/src/main/java/com/ung/todolistnotes/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     //private final static String TAG = "MainActivity";
     public static ToDoList mToDoList;
     private List<Task> taskList;
+    public static CategoryReadWrite mCategoryReadWrite;
     private EditText mItemEditText;
     private TextView mItemListTextView;
     private TextView mItemNumTodayTextView;
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
         mToDoList = new ToDoList(this);
         taskList = mToDoList.getTaskList();
+        mCategoryReadWrite = new CategoryReadWrite(this);
 
         //starts the recycler view by passing in data through adapter and setting a LayoutManager to position the items
         llm = new LinearLayoutManager(this);
@@ -80,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                displayNum();
 
                 Snackbar.make(rvTasks, deletedTask.getDesc() + " deleted.", Snackbar.LENGTH_LONG).setAction("Undo", view -> {
                     taskList.add(position, deletedTask);
@@ -92,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     displayNum();
                 }).show();
-
+                displayNum();
             }
         }).attachToRecyclerView(rvTasks);
 
@@ -161,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             // Attempt to load a previously saved list
             mToDoList.readFromFile();
+            mCategoryReadWrite.readFromFile();
             adapter.notifyDataSetChanged();
             //displayList();
             displayNum();
@@ -175,9 +177,8 @@ public class MainActivity extends AppCompatActivity {
         Task[] items = mToDoList.getItems();
         int u = items.length;
         String numItems = Integer.toString(u);
-
-        mItemNumTodayTextView.setText(numItems);
-        mItemNumOverdueTextView.setText(numItems);
+        mItemNumTodayTextView.setText(Integer.toString(mToDoList.GetNumDueToday()));
+        mItemNumOverdueTextView.setText(Integer.toString(mToDoList.GetNumOverdue()));
         mItemNumTextView.setText(numItems);
     }
 
@@ -186,7 +187,8 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         try {
-            // Save list for later
+            // Save list for laterm
+            mCategoryReadWrite.saveToFile();
             mToDoList.saveToFile();
         }
         catch (IOException ex) {
